057f0a562d86c4bf3b670ecb2bc78c2175b2d7aa
Srepin <px1099@gmail.com>
2024-09-27 10:13:38 +0800

    import template codes

---
+++ utils/golang/template.go
@@ -0,0 +1,17 @@
+//go:build ignore
+package main
+
+// import "fmt"
+
+func solve() {
+
+}
+
+func main() {
+	tc := 1
+	// fmt.Scan(&tc)
+	for t := 1; t <= tc; t++ {
+		// fmt.Print("Case ", t, ": ")
+		solve()
+	}
+}
---
+++ utils/python/template.py
@@ -0,0 +1,9 @@
+def solve():
+    pass
+
+
+if __name__ == "__main__":
+    tc = 1
+    # tc = int(input())
+    for _ in range(tc):
+        solve()
